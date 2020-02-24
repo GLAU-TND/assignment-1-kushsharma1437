c90c9e103d65ce1e021ed3550d707cf4a831ae79
kushs <kush.sharma_cs18@gla.ac.in>
2020-02-24 22:48:29 +0530

    ..

--- src/main/main.java
+++ src/main/main.java
@@ -1,4 +1,35 @@
 package main;
+import defination.Menu;
 
-public class main {
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        boolean flag=true;
+        Menu menu=new Menu();
+        while (flag) {
+
+            int n = menu.showChoice();
+            switch (n) {
+                case 1:
+                    menu.addChoice();
+                    break;
+                case 2:
+                    menu.viewChoice();
+                    break;
+                case 3:
+                    menu.searchChoice();
+                    break;
+                case 4:
+                    menu.deleteChoice();
+                    break;
+                case 5:
+                    menu.exitChoice();
+                    flag=false;
+                    break;
+
+            }
+        }
+    }
 }
